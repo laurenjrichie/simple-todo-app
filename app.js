e82aa0da7c02b5b29b9d38824ce56c354ce2ad86
laurenjrichie <laurenjrichie@gmail.com>
2015-01-22 15:47:02 -0800

    refactored

--- app.js
+++ app.js
@@ -1,43 +1,48 @@
 var itemCount = 0
 
-$("body").append("<h1>Todo App</h1>");
-$("body").append("<input type='text' placeholder='what do you need to learn?'>");
-$("body").append("<ul></ul>");
-$("body").append("<p>" + itemCount + " items left</p>");
-$("p").attr("id", "itemsLeft");
-$("body").append("<button>Delete completed</button>");
-$("button").attr("type", "button");
-$("button").attr("id", "deleteAll");
+function init() {
+  $("body").append("<h1>Todo App</h1>");
+  $("body").append("<input type='text' placeholder='what do you need to learn?'>");
+  $("body").append("<ul></ul>");
+  $("body").append("<p>" + itemCount + " items left</p>");
+  $("p").attr("id", "itemsLeft");
+  $("body").append("<button>Delete completed</button>");
+  $("button").attr("type", "button");
+  $("button").attr("id", "deleteAll");
+  $('input').keydown(pressEnter);
+  $('ul').on('click', 'span', deleteLi);
+  $('ul').on('click', 'li', completeLi);
+  $('button').click(deleteCompleted);
+}
 
-// add li
-$('input').keydown(function(e) {
-  var key = e.which;
-  if (key == 13) {
-    var value = $(this).val();
-    $("ul").append("<li>" + value + "<span class='delete'>Delete</span></li>");
-    // make placeholder reappear / value clear out?
-    itemCount += 1;
-    $("#itemsLeft").html(itemCount + " items left");
-  }
-});
+function pressEnter(key) {
+  if (key.which == 13) {appendLi(this)};
+}
 
-// delete individual li
-$('ul').on('click', 'span', function() {
-  if ($(this).parent().hasClass("completed")) {
-    itemCount += 1;
-  }
+function appendLi(element) {
+  var value = $(element).val();
+  $("ul").append("<li>" + value + "<span class='delete'>Delete</span></li>");  // make placeholder reappear / value clear out?
+  countItems("up");
+}
+
+function deleteLi() {
+  if ($(this).parent().hasClass("completed")) {countItems("up")}
   $(this).closest('li').remove();
-  $("#itemsLeft").html(itemCount + " items left");
-});
+}
 
-// cross out li
-$('ul').on('click', 'li', function() {
+function completeLi() {
   $(this).attr('class', 'completed');
-  itemCount -= 1;
-  $("#itemsLeft").html(itemCount + " items left");
-});
+  countItems("down");
+}
 
-// delete completed button
-$('button').click(function(e) {
+function deleteCompleted() {
   $(".completed").remove();
-});
+}
+
+function countItems(operator) {
+  if (operator === "down") {itemCount -= 1}
+  else {itemCount += 1}
+  $("#itemsLeft").html(itemCount + " items left");
+}
+
+window.onLoad = init();
